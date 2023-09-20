b7e99103eb7b900300f75499e960fa881106432f
Markus Brandt Højgaard <mbrh@itu.dk>
2023-09-20 12:55:04 +0200

    det er skide godt

--- Homework2.go
+++ Homework2.go
@@ -1,11 +1,12 @@
 package main
 import "fmt"
 import "math/rand"
+import "time"
 
 //Convention: sequnceX, sequnceY, data
 
 func main() {
-	var IPv4 = make(chan []int, 100);
+	var IPv4 = make(chan []int);
 
 	go Client(IPv4);
 	go Server(IPv4);
@@ -15,7 +16,17 @@ func main() {
 func Client (IPv4 chan []int) {
 	sequenceX := rand.Intn(3000000000);
 	shake1 := []int{sequenceX, 0}
-	IPv4 <- shake1
+	loop:
+	for{
+		select{
+		case IPv4 <- shake1:
+			fmt.Println("sendt")
+			break loop
+		case <- time.After(2 * time.Second):
+			fmt.Println("client not response")
+			break loop
+		}
+	}
 	shake2 := <- IPv4
 	if shake2[0] == sequenceX + 1  {
 		sequenceX := sequenceX + 1
@@ -31,13 +42,26 @@ func Server (IPv4 chan []int ) {
 	shake1 := <- IPv4
 	ackX := shake1[0] + 1;
 	shake2 := []int{ackX, sequenceY};
-	IPv4 <- shake2;
+	loop:
+	for{
+		select{
+		case IPv4 <- shake2:
+			fmt.Println("sendt2")
+			break loop
+		case <- time.After(2 * time.Second):
+			fmt.Println("client not response")
+			break loop
+		}
+	}
 	shake3 := <- IPv4
-	if shake3[0] == sequenceY + 1 { //Connection established
+	if shake3[1] == sequenceY + 1 { //Connection established
 		ackX = ackX + 1
 		fmt.Println("Connection established succesfully")
-		request1 := <- IPv4
+		// request1 := <- IPv4
 	} else {fmt.Println("Connection access denied: error 406")}
+
+	
 
 	// Rækkefølge, ikke være dobbelt, tjekke alle pakker er der
 }
+
--- README.txt
+++ README.txt
@@ -1,9 +1,23 @@
 a) What are packages in your implementation? What data structure do you use to transmit data and meta-data?
+    We donut use metadata, freaks
 
 b) Does your implementation use threads or processes? Why is it not realistic to use threads?
+   We use processes. For the simulation of the TCP these are sufficient enough to demonstrate how multiple 
+   tasks can be managed concuretly. To create a more realalistic simulation we would need to use two 
+   machines and actual network sockets - however this is a complex task.
 
 c) In case the network changes the order in which messages are delivered, how would you handle message re-ordering?
+    We  observe the sequence numbers. If one is missing - we would simply send a request to resend the data. 
 
 d) In case messages can be delayed or lost, how does your implementation handle message loss?
 
+
 e) Why is the 3-way handshake important?
+   The handshake is important to ensure both parties are ready to transfer data. Because this is a full duplex, we must
+   ensure that both client and server is ready - we do this by requesting and acknowleding. The other thing we need to do
+   is star the sequence for both client and server. 
+   Clint sends request (sequenceStart)-> 
+
+   Connection is succesful
+   Start the sequence 
+
