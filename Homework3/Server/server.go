6cced8ee8832f1bc187e3d8e3315e7ab524c403d
Allan <asia@itu.dk>
2023-10-11 13:31:12 +0200

    add server dontent

---
+++ Homework3/Server/server.go
@@ -0,0 +1,49 @@
+package Server
+
+import (
+
+	// This has to be the same as the go.mod module,
+	// followed by the path to the folder the proto file is in.
+	"context"
+	"net"
+	"unicode/utf8"
+
+	gRPC "github.com/palleBrandt/DistributedSystemHW/tree/main/Homework3/proto"
+	"google.golang.org/grpc"
+)
+
+func main(){
+	list, _ := net.Listen("tcp", "localhost:5400")
+	grpcServer := grpc.NewServer(opts...)
+
+
+	server := &Server{
+		streamMessage: make ([]*gRPC.Message, 0,2)}
+	
+	gRPC.RegisterChittyChatServer(grpcServer, server)
+	grpcServer.Serve(list)
+}
+
+
+
+type Server struct {
+    // an interface that the server type needs to have
+    gRPC.UnimplementedChittyChatServer
+    streamMessage 					[]*gRPC.Message;
+    // here you can implement other fields that you want
+}
+
+func (s *Server) Publish (ctx context.Context, message *gRPC.Message) (*gRPC.Message, error) {
+    // some code here
+
+	if 128 < utf8.RuneCountInString(message.Message){
+		s.streamMessage = append(s.streamMessage, message);
+		succesMessage := &gRPC.Message{
+    	Message: "fjing fjong ding dong"}
+		return succesMessage, nil
+	} else {
+		errorMessage := &gRPC.Message{
+    	Message: "Your shit is too long mf"}
+		return errorMessage, nil
+	}
+}
